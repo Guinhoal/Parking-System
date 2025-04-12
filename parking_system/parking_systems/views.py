9076065f43e2beee2f9253f627ef35540ebfc904
Thiago Arthur <thiagodevprof@gmail.com>
2025-04-12 15:28:10 -0300

    Views and Templates implemented

--- parking_system/parking_systems/views.py
+++ parking_system/parking_systems/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'parking_systems/index.html') 
+
+def dashboard(request):
+    return render(request, 'parking_systems/dashboard.html')
+
